06aa60b1a2d9bbc5bb7bdc7ff5460dba879001c7
SaiHemanthBR <sh.bheemreddy@icloud.com>
2020-03-31 19:24:56 +0530

    Solved popping sound problem

--- Exps/Havana.py
+++ Exps/Havana.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.io.wavfile import write
 import sounddevice as sd
+import matplotlib.pyplot as plt
 
 notes_frq = {"C": 261.63, "C#": 277.18, "D": 293.66, "D#": 311.13, "E": 329.63, "F": 349.23, "F#": 369.99, "G": 392, "G#": 415.3, "A": 440, "A#": 466.16, "B": 493.88, " ": 0}
 
@@ -42,24 +43,29 @@ notes = [
 "B", " ", " ", "A", "A", "B", " ", "A", "G", " ", "B", "F#",
 "G", "F#", "G", "F#", "G", "E"
 ]
-freqs = [notes_frq[note] for note in notes]
+filename = "Havana.wav"
 
+print(f"Synthesizing {filename}")
+freqs = [notes_frq[note] for note in notes]
 sps = 44100
 total_duration = 80
 duration = total_duration / len(freqs)
 
 waveform = np.array([])
 
-for freq in freqs:
-    each_sample_no = np.arange(duration * sps)
+for i, freq in enumerate(freqs):
+    cduration = (int(duration * freq) / freq) if freq != 0 else duration
+    each_sample_no = np.arange(cduration * sps)
     waveform = np.append(waveform, np.sin(2 * np.pi * each_sample_no * freq / sps))
-    each_sample_no_silent = np.zeros(int(0.001 * sps))
-    waveform = np.append(waveform, np.sin(each_sample_no_silent))
-
 waveform = waveform * 0.4
-write("havana.wav", sps, np.int16(waveform * 32767))
-print("saved")
 
-print("playing")
+write(filename, sps, np.int16(waveform * 32767))
+print(f"Saved as {filename}")
+
+print(f"Playing {filename}")
 sd.play(waveform, sps, blocking=True)
 sd.stop()
+
+if input("Do you want to see Waveform Plot?\nYour Computer may freeze... (Y/n): ").lower() == 'y':
+    plt.plot(waveform)
+    plt.show()
